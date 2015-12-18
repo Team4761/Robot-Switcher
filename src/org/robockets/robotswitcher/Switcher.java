808efdf8060c51127d24db7ef0b01abab64f3a3e
jakebacker <jbacker42@gmail.com>
2015-12-17 19:24:53 -0500

    Fix syntax problem

--- src/org/robockets/robotswitcher/Switcher.java
+++ src/org/robockets/robotswitcher/Switcher.java
@@ -5,16 +5,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Switcher {
 	public int currentRobot;
 	
-	public Switcher(){
+	public Switcher() {
 		currentRobot = getRobot();
 	}
 	
-	public int getRobot(){
+	public int getRobot() {
 		currentRobot = (int) SmartDashboard.getNumber("RobotNumber");
 		return currentRobot;
 	}
 	
-	public void initRobotNumber(){
+	public void initRobotNumber() {
 		SmartDashboard.putNumber("RobotNumber", 0); //TEMP
 	}	
 	
